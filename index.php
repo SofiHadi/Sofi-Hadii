7ff9c5a8b6999b6f79fbda1e477c5c03f68de858
SofiHadi <sofihadi813@gmail.com>
2026-04-29 20:43:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+echo "Hello Word":
+
+
+$data= 'Pemrograman web'
+echo "<br />";
+echo $data:
+$number = 10:number;
+echo $
+
+$dataMahasiswa = ['Sofi', 'Hadi', 'Permana']:
+array_push($dataMahasiswa, 'Sofi'):
+echo '<br />';
+echo $dataMahasiswa = [3];
+
+$a = 30;
+$b = 35;
+
+echo '<br />';
+echo $a + $b;
+
+if ($a != $b) {
+    echo '<br />';
+    echo 'benar (true)';
+}
+else {
+    echo '<br />';
+    echo 'salah (false)';
+}
+
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php $dataMahasiswa?>
+
+
+
+
+
+</body>
+</html>
